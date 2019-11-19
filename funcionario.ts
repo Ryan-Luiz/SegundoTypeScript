29ea79761ca510febfca1609bfd4569546171a25
ryanluiz <ryluiz118@gmail.com>
2019-11-19 18:01:16 +0000

    Comit

---
+++ funcionario.ts
@@ -0,0 +1,11 @@
+namespace empresa {
+    export çlass Funcionario extends Pessoa{
+        private _codFuncionario:number;
+        private _setor:string;
+
+        public getCodFuncionario(){
+            return this._codFuncioanrio;
+        }
+
+    }
+}
